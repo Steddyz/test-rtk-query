3aadbb9ffa9df3314285fd041628485ac1a4ed8c
Steddy <qwesteddyoff@gmail.com>
2024-08-13 21:14:47 +0300

    add build mutation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import { useState } from "react";
-import { useGetGoodsQuery } from "./store";
+import { useGetGoodsQuery, useAddProductMutation } from "./store";
 
 function App() {
   const [count, setCount] = useState("");
   const { data, isLoading } = useGetGoodsQuery(count);
+  const [newProduct, setNewProduct] = useState("");
+  const [addProduct, { isError, error }] = useAddProductMutation();
+
+  const handleAddProduct = async () => {
+    if (newProduct !== "") {
+      await addProduct({ name: newProduct }).unwrap();
+      setNewProduct("");
+    }
+  };
+
+  const buttonAddProduct = async (event) => {
+    if (event.key === "Enter") {
+      await handleAddProduct();
+    }
+  };
 
   return (
     <div className="App">
       {isLoading && <div>Loading...</div>}
+      {isError && <div>Error: {error.message}</div>}
 
-      <select value={count} onChange={(e) => setCount(e.target.value)}>
-        <option value="">All</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-      </select>
+      <div>
+        <input
+          autoFocus
+          value={newProduct}
+          onChange={(e) => setNewProduct(e.target.value)}
+          onKeyDown={buttonAddProduct}
+        />
+        <button onClick={handleAddProduct}>Create</button>
+      </div>
+      <div>
+        <select value={count} onChange={(e) => setCount(e.target.value)}>
+          <option value="">All</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
 
       <ul>
         {data?.map((item) => (
