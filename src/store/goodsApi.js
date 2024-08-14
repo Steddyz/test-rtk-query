e13f01f51635645d87bf1f808523adb4d24dd94f
Steddy <qwesteddyoff@gmail.com>
2024-08-14 09:57:10 +0300

    add auto-update

--- src/store/goodsApi.js
+++ src/store/goodsApi.js
@@ -2,12 +2,20 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const goodsApi = createApi({
   reducerPath: "goodsApi",
+  tagTypes: ["Products"],
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3001/",
   }),
   endpoints: (build) => ({
     getGoods: build.query({
       query: (limit = "") => `goods?${limit && `_limit=${limit}`}`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Products", id })),
+              { type: "Products", id: "LIST" },
+            ]
+          : [{ type: "Products", id: "LIST" }],
     }),
     addProduct: build.mutation({
       query: (body) => ({
@@ -15,6 +23,7 @@ export const goodsApi = createApi({
         method: "POST",
         body,
       }),
+      invalidatesTags: [{ type: "Products", id: "LIST" }],
     }),
   }),
 });
